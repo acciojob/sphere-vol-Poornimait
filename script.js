161870d2f812ccddc3122548e1117f2d8364895d
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-05 20:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function volume_sphere() {
     //Write your code here
 	let radiusinput=document.getElementById("radius");
-	let radius=parseFloat(radiusinput.value);
+	let radius=radiusinput.value;
 	var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
     var volumeField = document.getElementById("volume");
-	if (!isNaN(volume)) {
-        volumeField.value = volume.toFixed(4);
-    } 
+	
+    volumeField.value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
