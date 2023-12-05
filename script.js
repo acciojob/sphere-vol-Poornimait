04d1c4df68b9957eb0641ed716090e8e95a86e74
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-05 21:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function volume_sphere() {
     //Write your code here
 	const radius = document.getElementById('radius').value;
 	const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-	document.getElementById('volume').value = volume;
+	const roundedVolume = parseFloat(volume.toFixed(4));
+	document.getElementById('volume').value = roundedVolume;
 	return false;
 } 
 
